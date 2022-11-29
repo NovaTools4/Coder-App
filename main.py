f9d27df1fd85027a5949468ad1f4135258dde351
NovaTools4 <94282470+NovaTools4@users.noreply.github.com>
2022-11-29 14:27:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -39,35 +39,53 @@ class Function(ScreenManager):
 				name_prompt = str(self.ids.name_prompt.text)
 				prompt = str(self.ids.prompt.text)
 				try:
-					url = requests.get("https://deepai.nova8593.repl.co/coder?text={}".format(prompt)).text
-					response = json.loads(url)
-					code = response["info"]["Order"]
+					try:
+						url = requests.get("https://deepai.nova8593.repl.co/coder?text={}".format(prompt)).text
+						response = json.loads(url)
+						code = response["info"]["Order"]
+					
+						if os.path.exists("/sdcard/Coder"):
+								with open(f"/sdcard/Coder/{name_prompt}.py", "w") as file:
+									file.write(str(code))
+								self.ids.result.text="The file has been saved to : "+"/sdcard/Coder/"+name_prompt+".py"
+								self.ids.result_title.text="RESULT"
+								self.ids.result_title.color="cyan"
+								self.ids.result.color="green"
+								self.ids.all.text=""
+								root.current="result"
+						else:
+								os.mkdir("/sdcard/Coder/")
+								with open(f"/sdcard/Coder/{name_prompt}.py", "w") as file:
+									file.write(str(code))
+								self.ids.result.text="The file has been saved to : "+"/sdcard/Coder/"+name_prompt+".py"
+								self.ids.result_title.text="RESULT"
+								self.ids.result_title.color="cyan"
+								self.ids.result.color="green"
+								self.ids.all.text=""
+								root.current="result"
+							
+					except requests.exceptions.SSLError:
+							if not self.dialog:
+								self.dialog = MDDialog(
+									title = "Coder",
+									text = "Looks like you didn't download the app from the store or maybe your network is not secure.",
+									buttons =[
+										MDRectangleFlatButton(
+											text="Ok", on_release = self.neat_dialog
+											),
+										],
+									)
+					
+							self.dialog.open()
 				
-					if os.path.exists("/sdcard/Coder"):
-							with open(f"/sdcard/Coder/{name_prompt}.py", "w") as file:
-								file.write(str(code))
-							self.ids.result.text="The file has been saved to : "+"/sdcard/Coder/"+name_prompt+".py"
-							self.ids.result_title.text="RESULT"
-							self.ids.result_title.color="cyan"
-							self.ids.result.color="green"
-							self.ids.all.text=""
-							root.current="result"
-					else:
-							os.mkdir("/sdcard/Coder/")
-							with open(f"/sdcard/Coder/{name_prompt}.py", "w") as file:
-								file.write(str(code))
-							self.ids.result.text="The file has been saved to : "+"/sdcard/Coder/"+name_prompt+".py"
-							self.ids.result_title.text="RESULT"
-							self.ids.result_title.color="cyan"
-							self.ids.result.color="green"
-							self.ids.all.text=""
-							root.current="result"
-						
-				except requests.exceptions.SSLError:
-						if not self.dialog:
+				except requests.exceptions.ConnectionError:
+					if not self.dialog:
 							self.dialog = MDDialog(
-								title = "Coder",
-								text = "Looks like you didn't download the app from the store or maybe your network is not secure.",
+								title = "Error",
+								text = '''
+Oops, something went wrong.
+Please try again later
+								''',
 								buttons =[
 									MDRectangleFlatButton(
 										text="Ok", on_release = self.neat_dialog
@@ -75,7 +93,8 @@ class Function(ScreenManager):
 									],
 								)
 				
-						self.dialog.open()
+					self.dialog.open()
+
 
 			else:
 				self.ids.result.text="Input Field is Empty"
